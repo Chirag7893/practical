6d8bfe8600a7286acb09a24c4dca6cfd62cd953a
Chirag Patel <patelchirag7893@gmail.com>
2022-03-16 11:14:32 +0530

    Initial Commit

---
+++ Practical_TheThetaLab/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Practical_TheThetaLab
+//
+//  Created by Chirag Patel on 16/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
